ab7f9f6b37181883c8d85fbafe78963b6b00c3c0
David Strauß <david@strauss.io>
2019-11-17 19:33:56 +0100

    Implement addition of one third and one third for fraction

--- tests/unit/dummy_test.rb
+++ tests/unit/dummy_test.rb
@@ -20,4 +20,8 @@ class DummyTitle < UnitTest
   def test_addition_of_one_and_one
     assert_equal Fraction.new('1') + Fraction.new('1'), Fraction.new('2')
   end
+
+  def test_addition_of_one_third_and_one_third
+    assert_equal Fraction.new('1/3') + Fraction.new('1/3'), Fraction.new('2/3')
+  end
 end
